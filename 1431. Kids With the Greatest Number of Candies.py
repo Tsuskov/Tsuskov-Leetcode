b14d19658da195290ad48aab9c9e82a5a6255671
Tsuskov <fen_dregs_0g@icloud.com>
2024-10-20 17:33:01 +0200

    0ms

--- 1431. Kids With the Greatest Number of Candies.py
+++ 1431. Kids With the Greatest Number of Candies.py
@@ -3,6 +3,6 @@ from typing import List
 
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        max_candies = max(candies)
-        return [candy + extraCandies >= max_candies for candy in candies]
+        max_candies = max(candies) # Find the maximum number of candies
+        return [candy + extraCandies >= max_candies for candy in candies] # Return a list of booleans indicating whether each kid can have the greatest number of candies
     
