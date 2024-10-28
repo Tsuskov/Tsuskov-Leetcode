2c856c0ad3d131ac816f6e3b16b1083d8abb1cab
Tsuskov <fen_dregs_0g@icloud.com>
2024-10-28 17:49:51 +0100

    w

---
+++ 875.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def canFinish(piles, k, h):
+            hours = 0
+            for pile in piles:
+             hours += (pile + k - 1) // k  
+            return hours <= h
+
+        left, right = 1, max(piles)
+        while left < right:
+            mid = (left + right) // 2
+            if canFinish(piles, mid, h):
+             right = mid
+            else:
+             left = mid + 1
+        return left
