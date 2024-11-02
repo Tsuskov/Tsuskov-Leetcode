3ad4bffc240b56260d7b3d8968fcb646a42c4bd5
Tsuskov <fen_dregs_0g@icloud.com>
2024-11-02 22:25:30 +0100

    Add Solution class with maxProfit method to 714.py

---
+++ 714.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        if n == 0:
+            return 0
+        
+        cash, hold = 0, -prices[0]
+        
+        for i in range(1, n):
+            cash = max(cash, hold + prices[i] - fee)
+            hold = max(hold, cash - prices[i])
+        
+        return cash
