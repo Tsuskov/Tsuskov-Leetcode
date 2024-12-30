20a8e0e3af7fce614d7e958aa9d67fd28abe490c
Tsuskov <fen_dregs_0g@icloud.com>
2024-12-30 15:27:51 +0100

    Add solutions for special integer finder and Tic Tac Toe game

---
+++ 25percent.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        threshold = n // 4
+        for i in range(n - threshold):
+            if arr[i] == arr[i + threshold]:
+             return arr[i]
+        return -1
