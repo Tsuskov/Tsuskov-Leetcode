163361bc7bfa3f7945dd0371f0ded488f65edc87
Tsuskov <fen_dregs_0g@icloud.com>
2025-08-25 14:34:04 +0200

    Add convertToBase7 method to convert an integer to its base 7 representation

---
+++ 504.py
@@ -0,0 +1,11 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        negative = num < 0
+        num = abs(num)
+        base7 = ""
+        while num > 0:
+            base7 = str(num % 7) + base7
+            num //= 7
+        return "-" + base7 if negative else base7
