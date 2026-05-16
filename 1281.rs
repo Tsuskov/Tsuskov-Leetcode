75e45421e373f3d91379c594497c645063afa267
Tsuskov <fen_dregs_0g@icloud.com>
2026-05-16 10:38:44 +0200

    geilo kösung

---
+++ 1281.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn subtract_product_and_sum(n: i32) -> i32 {
+        let mut product = 1;
+        let mut sum = 0;
+        let mut num = n;
+        
+        while num > 0 {
+            let digit = num % 10;
+            product *= digit;
+            sum += digit;
+            num /= 10;
+        }
+        
+        product - sum
+    }
+}
